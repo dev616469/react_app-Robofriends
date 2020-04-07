a44d40b474371506b2c473fd1dcfae0b96021372
dev616469 <i616469747961@gmail.com>
2020-04-07 14:49:35 +0530

    Initial COmmit of the App

---
+++ src/components/Scroll.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Scroll(props) {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "5px solid black",
+        height: "800px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+}
+export default Scroll;
